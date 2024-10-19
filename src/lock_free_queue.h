005b447fcb8778fd5ef244a02a9b5c405e8cad72
Begali Aslonov <engineerbekali@gmail.com>
2024-10-20 00:03:00 +0200

    lock free queue infra setup

--- src/lock_free_queue.h
+++ src/lock_free_queue.h
@@ -9,4 +9,27 @@ class LockFreeQueue {
     private:
         std::array<T, Size> buffer;
         std::atomic<size_t> head{0};
-}
+        std::atomic<size_t> tail{0};
+    
+    public:
+        bool enqueue(const T& item) {
+            size_t current_tail = tail.load(std::memory_order_relaxed);
+            size_t next_tail = (current_tail + 1) % Size;
+            if (next_tail == head.load(std::memory_order_acquire))
+                return false; // Queue is full
+            buffer[current_tail] = item;
+            tail.store(next_tail, std::memory_order_release);
+            return true;
+        }
+
+        bool dequeue(T& item) {
+            size_t current_head = head.load(std::memory_order_relaxed);
+            if (current_head == tail.load(std::memory_order_acquire))
+                return false; // Queue is empty
+            item = buffer[current_head]
+            head.store((current_head + 1) % Size, std::memory_order_release);
+            return true;
+        }
+};
+
+#endif // LOCK_FREE_QUEUE_H
